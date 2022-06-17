6b4081e3001316385b624cf8ebe8130987dcd177
wnrome21 <nateromero001@gmail.com>
2022-06-16 17:41:32 -0700

    Added more components to the user/components folder

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './UserItem.css';
+
+const UserItem = (props) => {
+    return ( 
+        <div>
+
+        </div>
+     );
+}
+ 
+export default UserItem;
---
+++ src/user/components/UsersList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './UsersList.css';
+
+const UsersList = (props) => {
+    if ( props.items)
+    return ( 
+        <div>
+
+        </div>
+     );
+}
+ 
+export default UsersList;
