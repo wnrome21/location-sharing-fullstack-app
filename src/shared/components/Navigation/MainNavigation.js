b920f977ff276b80ac7bf522624a044fb78a7a9f
wnrome21 <nateromero001@gmail.com>
2022-06-18 10:52:46 -0700

    added the side drawer component and created a portal for the side drawer to be rendered in the body of the app.

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -2,22 +2,33 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
 
 import "./MainNavigation.css";
 
 const MainNavigation = (props) => {
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <Link to="/">
-        <h1 className="main-navigation__title">Location Sharer</h1>
-      </Link>
-      <nav>...</nav>
-    </MainHeader>
+    <React.Fragment>
+      <SideDrawer>
+        <nav className="main-navigation__drawer">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+        <Link to="/">
+          <h1 className="main-navigation__title">Location Sharer</h1>
+        </Link>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
   );
 };
 
