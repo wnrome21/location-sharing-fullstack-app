7229f53e139ffe2cc29b63373155213147e3279b
wnrome21 <nateromero001@gmail.com>
2022-06-18 15:20:24 -0700

    added the UserPlaces page and that UserPlaces renders out the placeItem and the PlaceList. Added then with styling also.

--- src/App.js
+++ src/App.js
@@ -8,21 +8,25 @@ import {
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UserPlaces from "./places/pages/UserPlaces";
 
 function App() {
   return (
     <Router>
       <MainNavigation />
       <main>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
       </main>
     </Router>
   );
---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import PlaceList from '../components/PlaceList';
+
+
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world!',
+        imageURL: 'https://media.timeout.com/images/101705309/750/422/image.jpg',
+        address: '20 W 34th St., New York, NY 10001',
+        locations: {
+            lat: 40.7484405,
+            lng: -73.9878584
+        },
+        creator: 'u1'
+    },
+    {
+        id: 'p2',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world!',
+        imageURL: 'https://media.timeout.com/images/101705309/750/422/image.jpg',
+        address: '20 W 34th St., New York, NY 10001',
+        locations: {
+            lat: 40.7484405,
+            lng: -73.9878584
+        },
+        creator: 'u2'
+    }
+]
+
+
+const UserPlaces = () => {
+   const userId = useParams().userId;
+    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId)
+    return ( 
+        <PlaceList items={loadedPlaces} />
+        );
+}
+ 
+export default UserPlaces;
