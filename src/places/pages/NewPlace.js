a6b694e6e60576a17f083f240c354dd13fbb51f9
wnrome21 <nateromero001@gmail.com>
2022-06-21 16:23:53 -0700

    added an input component with form validation. form validation is stored in the util folder in the validators file.

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
+
+import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import "./NewPlace.css";
 
 const NewPlace = () => {
-    return ( 
-        <h2>new place works!</h2>
-     );
-}
- 
+  return (
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please Enter A Valid Title"
+      />
+    </form>
+  );
+};
+
 export default NewPlace;
