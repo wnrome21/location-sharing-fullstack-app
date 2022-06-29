8dc2c068ea4d192bda248e1b08af4885673ff9c8
wnrome21 <nateromero001@gmail.com>
2022-06-29 02:51:42 -0700

    fixed the routing to the my places nav links. Will now take you to your list of places. Now if you are not signed in to the account that created the place, or if you are not logged you will not be able to see the edit and delete buttons.

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -16,12 +16,12 @@ const NavLinks = () => {
       </li>
       {auth.isLoggedIn && (
         <li>
-          <NavLink to="/u1/places">MY PLACES</NavLink>
+          <NavLink to={`/${auth.userId}/places`}>MY PLACES</NavLink>
         </li>
       )}
       {auth.isLoggedIn && (
         <li>
-          <NavLink to="/places/new">ADD PLACES</NavLink>
+          <NavLink to="/places/new">ADD PLACE</NavLink>
         </li>
       )}
       {!auth.isLoggedIn && (
