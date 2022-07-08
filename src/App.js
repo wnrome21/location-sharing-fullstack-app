e441a6a7cdde8efb5427feb22baffeaa60a5b27a
wnrome21 <nateromero001@gmail.com>
2022-07-08 15:57:15 -0700

    added the image upload for a new user on the front end and on the back end. Now the user item component will display the image that has been uploaded for that specific user. The image url is saved in the database and the image is saved in node.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useCallback } from "react";
+
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
   Switch,
 } from "react-router-dom";
+
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
